5d56a72864cffdeeae16df681d10b93b53ac94f8
mizuhar <mizuhar@abv.bg>
2023-11-14 18:43:49 +0200

    Add Header component and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import Header from "./components/Header/Header"
+
 
 function App() {
 
   return (
-    <>
-    <h1>Hello world!</h1>
+    <div id="box">
+
+
+   <Header></Header>
       
-    </>
+    </div>
   )
 }
 
