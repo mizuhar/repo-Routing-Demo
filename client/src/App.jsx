b3e1882dbe6429a59799daf3aef00e376abc400b
mizuhar <mizuhar@abv.bg>
2023-11-14 19:54:32 +0200

    Add register login and create page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,10 @@
+import { Routes,Route } from "react-router-dom"
+import CreateGame from "./components/CreateGame/CreateGame"
+import GameList from "./components/GameList/GameList"
 import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
 
 
 function App() {
@@ -6,8 +12,18 @@ function App() {
   return (
     <div id="box">
 
-
    <Header></Header>
+
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/games" element={<GameList/>}></Route>
+        <Route path="/create" element={<CreateGame/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+   
+
+
       
     </div>
   )
