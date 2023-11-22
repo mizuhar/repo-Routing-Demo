60da65ebdd5e8d7be276e9eec51a011e98785a93
mizuhar <mizuhar@abv.bg>
2023-11-22 08:22:29 +0200

    Create Custum Hook

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,13 @@
+import useForm from "../../hooks/useForm"
+
+
+
 export default function Login (){
+  const {values,onChange,onSubmit} = useForm({email: "", password:""})
     return (
         <>
         <section id="login-page" className="auth">
-  <form id="login">
+  <form id="login" onSubmit={onSubmit}>
     <div className="container">
       <div className="brand-logo" />
       <h1>Login</h1>
@@ -12,10 +17,22 @@ export default function Login (){
         id="email"
         name="email"
         placeholder="Sokka@gmail.com"
+        onChange={onChange}
+        value={values.email}
       />
       <label htmlFor="login-pass">Password:</label>
-      <input type="password" id="login-password" name="password" />
-      <input type="submit" className="btn submit" defaultValue="Login" />
+      <input 
+        type="password"
+        id="login-password"
+        name="password" 
+        onChange={onChange}
+        value={values.password}
+        />
+      <input 
+        type="submit" 
+        className="btn submit"
+        defaultValue="Login"
+        />
       <p className="field">
         <span>
           If you don't have profile click <a href="#">here</a>
