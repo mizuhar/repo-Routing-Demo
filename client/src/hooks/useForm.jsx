4eacc96bec63f6eb5a604e22890235dbeb25ee26
mizuhar <mizuhar@abv.bg>
2023-11-22 08:23:08 +0200

    Change Login to controlled form

---
+++ client/src/hooks/useForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export default function useForm(submiHandler,initialValue){
+    const [values,setValues] = useState(initialValue)
+   const onChange = (e)=>{
+    setValues(state => ({...state,[e.target.name]:[e.target.value]})
+
+   )}
+   const onSubmit =  (e) =>{
+       e.preventDefault()
+       submiHandler(values)
+
+   }
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
+}
